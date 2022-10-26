1ea2f8bc84becce11cabdc448c0b5bffb8a61462
Administrator1 <63410075@go.buu.ac.th>
2022-10-26 21:34:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BuildGrid043![Pic](https://user-images.githubusercontent.com/110089040/188232772-06edd6fb-4ed3-4780-ac64-650d879a92f6.PNG)
---
+++ app/src/main/java/com/example/buildgrid043/data/DataSource.kt
@@ -0,0 +1,29 @@
+package com.example.buildgrid043.data
+
+import com.example.buildgrid043.R
+import com.example.buildgrid043.model.Topic
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.hotto, 58, R.drawable.hotto),
+        Topic(R.string.sleeping, 121, R.drawable.sleeping),
+        Topic(R.string.angry, 15, R.drawable.angry),
+        Topic(R.string.boring, 191, R.drawable.boring),
+        Topic(R.string.cool, 10, R.drawable.cool),
+        Topic(R.string.cold, 9, R.drawable.cold),
+        Topic(R.string.cute, 180, R.drawable.cute),
+        Topic(R.string.fail, 29, R.drawable.fail),
+        Topic(R.string.fighting, 170, R.drawable.fighting),
+        Topic(R.string.hbd, 82, R.drawable.hbd),
+        Topic(R.string.nice, 56, R.drawable.nice),
+        Topic(R.string.ok, 14, R.drawable.ok),
+        Topic(R.string.Sad, 20, R.drawable.sad),
+        Topic(R.string.today, 130, R.drawable.today),
+        Topic(R.string.trythis, 78, R.drawable.trythis),
+        Topic(R.string.whatisthis, 69, R.drawable.whatisthis),
+        Topic(R.string.working, 180, R.drawable.working),
+        Topic(R.string.me, 63410043, R.drawable.me)
+
+    )
+}
+
